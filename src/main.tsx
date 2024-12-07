a605103be03db58cb50f8d497aef75ee19a064fd
Maria <mariaagon7@gmail.com>
2024-12-07 11:55:37 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client' // Nueva API de React 18
+import './index.css' // Tu archivo de estilos
+import App from './App.tsx' // Tu componente principal
 
+// Crear el "root" donde se renderiza la aplicación
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
