991511656d71bc3c9fd6007901f110c6617b582e
Maria <mariaagon7@gmail.com>
2024-12-08 10:06:58 +0100

    Updates

--- assets/status-tap-DaknycjI.js
+++ assets/status-tap-DaknycjI.js
@@ -1,3 +1,3 @@
-import{r as a,a as i,w as c}from"./index-Q-tpQiRe.js";import{f as d,s as l}from"./index8-Cwjbwquq.js";/*!
+import{r as a,a as i,w as c}from"./index-8sA9CEOP.js";import{f as d,s as l}from"./index8-CjPvo8Fv.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const p=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=d(o);t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{p as startStatusTap};
